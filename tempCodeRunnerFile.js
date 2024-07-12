3a5e5c7c55e80f8ec622751100626d830d7a96ef
mthtitumir <titumirgccc@gmail.com>
2024-07-12 17:41:29 +0600

    unnecessary file deleted

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-function filterAndMapMales(peopleArray) {
-//   return peopleArray
-//     .filter((person) => person.gender === "male")
-//     .map((person) => person.name);
-// }
-
-// // Call the function and print the result
-// const maleNames = filterAndMapMales(people);
-// console.log(maleNames);
